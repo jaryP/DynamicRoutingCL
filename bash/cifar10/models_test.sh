8d4adb960c3996c1863ab89bba668526e71aeb38
jary.pomponi <jarypomponi@gmail.com>
2023-10-20 12:14:04 +0200

    Old bash removed and env  updated

--- bash/cifar10/models_test.sh
+++ bash/cifar10/models_test.sh
@@ -10,12 +10,12 @@ der)
   python main.py +scenario=cil_cifar10_5tasks +model=resnet20 +training=cifar10_5 +method=der_default optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=1 method.beta=1 experiment=base1 method.mem_size=200
 ;;
 replay)
-  python main.py +scenario=cil_cifar10_5tasks +model=alexnet +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200 training.epochs=1
+  python main.py +scenario=cil_cifar10_5tasks +model=alexnet +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200
   python main.py +scenario=cil_cifar10_5tasks +model=resnet20 +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200
 ;;
 routing)
-  python main.py +scenario=cil_cifar10_5tasks +model=alexnet +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200 training.epochs=1
-  python main.py +scenario=cil_cifar10_5tasks +model=resnet20 +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200
+  python main.py +scenario=cil_cifar10_5tasks +model=routing_3l_convblock +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200
+#  python main.py +scenario=cil_cifar10_5tasks +model=resnet20 +training=cifar10_5 +method=replay_default optimizer=adam device=$DEVICE experiment=base1 method.mem_size=200
 ;;
 *)
   echo -n "Unrecognized method"
