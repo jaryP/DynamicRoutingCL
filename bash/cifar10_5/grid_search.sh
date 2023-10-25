197563e27ee64e8c5756da982043465d1ddef483
jary.pomponi <jarypomponi@gmail.com>
2023-10-25 17:02:15 +0200

    Modified save path

--- bash/cifar10_5/grid_search.sh
+++ bash/cifar10_5/grid_search.sh
@@ -55,9 +55,9 @@ routing)
 #  routing_3l_convblock_invusage
   for memory in 200 500 1000 2000
   do
-    for past_margin in 0.1 0.2 0.3 0.5
-    do
-      for past_margin_w in 1 0.5 0.1 0.01 0.001
+    for past_margin in 1 2 3 0.3 0.2
+     do
+      for past_margin_w in 1 0.5 0.1 0.01
       do
         for future_margin in 5
         do
@@ -67,7 +67,7 @@ routing)
                 do
                 for pt in task class
                 do
-              python main.py +scenario=cil_cifar10_5tasks +model=$MODEL +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE experiment=dev method.mem_size=$memory method.past_margin=$past_margin  method.future_margin=$future_margin method.past_task_reg=$past_margin_w method.future_task_reg=0.5 method.gamma=$gamma hydra=search model.path_selection_strategy=$pa model.prediction_mode=$p wandb_prefix=routing_model_search_
+              python main.py +scenario=cil_cifar10_5tasks +model=$MODEL +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE experiment=dev method.mem_size=$memory method.past_margin=$past_margin  method.future_margin=$future_margin method.past_task_reg=$past_margin_w method.future_task_reg=0.5 method.gamma=$gamma hydra=search model.path_selection_strategy=$pa model.prediction_mode=$pt wandb_prefix=routing_model_search_
             done
             done
             done
@@ -76,6 +76,7 @@ routing)
       done
     done
 ;;
+
 #icarl)
 #  python main.py +scenario=cil_cifar10_5tasks +model=resnet20 +training=cifar10_5 +method=icarl_2000 optimizer=sgd  training.device="$DEVICE" hydra.run.dir='./results/ci_cifar10/resnet20/icarl/icarl_2000/'
 #;;
--- main.py
+++ main.py
@@ -7,14 +7,9 @@ import warnings
 import hydra
 import numpy as np
 import torch
-import yaml
-from avalanche.benchmarks import data_incremental_benchmark, SplitCIFAR10
-from avalanche.evaluation.metrics import accuracy_metrics, bwt_metrics, \
-    timing_metrics, forgetting_metrics, MAC_metrics, StreamAccuracy, \
-    TrainedExperienceAccuracy
+from avalanche.benchmarks import data_incremental_benchmark
 from avalanche.logging import WandBLogger
 
-from avalanche.logging.text_logging import TextLogger
 from avalanche.training import DER
 from avalanche.training.plugins import EvaluationPlugin
 from omegaconf import DictConfig, OmegaConf
@@ -22,102 +17,13 @@ from torch.nn import CrossEntropyLoss
 
 from base.scenario import get_dataset_nc_scenario
 from models.base import get_cl_model
-from utils import TrainableParametersPlugin, ExperienceTrainableParameters, \
-    StreamTrainableParameters
 
-# from models.base import get_cl_model
 
-# class CustomTextLogger(StrategyLogger):
-#     def __init__(self, file=sys.stdout):
-#         super().__init__()
-#         self.file = file
-#         self.metric_vals = {}
-#
-#     def log_single_metric(self, name, value, x_plot) -> None:
-#         self.metric_vals[name] = (name, x_plot, value)
-#
-#     def _val_to_str(self, m_val):
-#         if isinstance(m_val, torch.Tensor):
-#             return '\n' + str(m_val)
-#         elif isinstance(m_val, float):
-#             return f'{m_val:.4f}'
-#         else:
-#             return str(m_val)
-#
-#     def print_current_metrics(self):
-#         sorted_vals = sorted(self.metric_vals.values(),
-#                              key=lambda x: x[0])
-#         for name, x, val in sorted_vals:
-#             if isinstance(val, UNSUPPORTED_TYPES):
-#                 continue
-#             val = self._val_to_str(val)
-#             print(f'\t{name} = {val}', file=self.file, flush=True)
-#
-#     def before_training_exp(self, strategy: 'SupervisedTemplate',
-#                             metric_values: List['MetricValue'], **kwargs):
-#         super().before_training_exp(strategy, metric_values, **kwargs)
-#         self._on_exp_start(strategy)
-#
-#     def before_eval_exp(self, strategy: 'SupervisedTemplate',
-#                         metric_values: List['MetricValue'], **kwargs):
-#         super().before_eval_exp(strategy, metric_values, **kwargs)
-#         self._on_exp_start(strategy)
-#
-#     def after_eval_exp(self, strategy: 'SupervisedTemplate',
-#                        metric_values: List['MetricValue'], **kwargs):
-#         super().after_eval_exp(strategy, metric_values, **kwargs)
-#         exp_id = strategy.experience.current_experience
-#         task_id = phase_and_task(strategy)[1]
-#         if task_id is None:
-#             print(f'> Eval on experience {exp_id} '
-#                   f'from {stream_type(strategy.experience)} stream ended.',
-#                   file=self.file, flush=True)
-#         else:
-#             print(f'> Eval on experience {exp_id} (Task '
-#                   f'{task_id}) '
-#                   f'from {stream_type(strategy.experience)} stream ended.',
-#                   file=self.file, flush=True)
-#         self.print_current_metrics()
-#         self.metric_vals = {}
-#
-#     def before_training(self, strategy: 'SupervisedTemplate',
-#                         metric_values: List['MetricValue'], **kwargs):
-#         super().before_training(strategy, metric_values, **kwargs)
-#         print('-- >> Start of training phase << --', file=self.file, flush=True)
-#
-#     def before_eval(self, strategy: 'SupervisedTemplate',
-#                     metric_values: List['MetricValue'], **kwargs):
-#         super().before_eval(strategy, metric_values, **kwargs)
-#         print('-- >> Start of eval phase << --', file=self.file, flush=True)
-#
-#     def after_training(self, strategy: 'SupervisedTemplate',
-#                        metric_values: List['MetricValue'], **kwargs):
-#         super().after_training(strategy, metric_values, **kwargs)
-#         print('-- >> End of training phase << --', file=self.file, flush=True)
-#
-#     def after_eval(self, strategy: 'SupervisedTemplate',
-#                    metric_values: List['MetricValue'], **kwargs):
-#         super().after_eval(strategy, metric_values, **kwargs)
-#         print('-- >> End of eval phase << --', file=self.file, flush=True)
-#         self.print_current_metrics()
-#         self.metric_vals = {}
-#
-#     def _on_exp_start(self, strategy: 'SupervisedTemplate'):
-#         action_name = 'training' if strategy.is_training else 'eval'
-#         exp_id = strategy.experience.current_experience
-#         task_id = phase_and_task(strategy)[1]
-#         stream = stream_type(strategy.experience)
-#         if task_id is None:
-#             print('-- Starting {} on experience {} from {} stream --'
-#                   .format(action_name, exp_id, stream),
-#                   file=self.file,
-#                   flush=True)
-#         else:
-#             print('-- Starting {} on experience {} (Task {}) from {} stream --'
-#                   .format(action_name, exp_id, task_id, stream),
-#                   file=self.file,
-#                   flush=True)
+def process_saving_path(sstring: str) -> str:
+    return '__'.join(s.split('=')[1] for s in sstring.split('__'))
 
+
+OmegaConf.register_new_resolver("process_saving_path", process_saving_path)
 OmegaConf.register_new_resolver("method_path", lambda x: x.split('.')[-1])
 
 
