ee11388c1b25896aa78725f9af8efb0f8a13507d
jary.pomponi <jarypomponi@gmail.com>
2023-10-25 17:51:29 +0200

    Grid search modified

--- bash/cifar10_5/grid_search.sh
+++ bash/cifar10_5/grid_search.sh
@@ -57,7 +57,7 @@ routing)
   do
     for past_margin in 1 2 3 0.3 0.2
      do
-      for past_margin_w in 1 0.5 0.1 0.01
+      for past_margin_w in 0.5 0.1 0.01
       do
         for future_margin in 5
         do
@@ -67,7 +67,7 @@ routing)
                 do
                 for pt in task class
                 do
-              python main.py +scenario=cil_cifar10_5tasks +model=$MODEL +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE experiment=dev method.mem_size=$memory method.past_margin=$past_margin  method.future_margin=$future_margin method.past_task_reg=$past_margin_w method.future_task_reg=0.5 method.gamma=$gamma hydra=search model.path_selection_strategy=$pa model.prediction_mode=$pt wandb_prefix=routing_model_search_
+              python main.py +scenario=cil_cifar10_5tasks +model=$MODEL +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE experiment=dev method.mem_size=$memory method.past_margin=$past_margin  method.future_margin=$future_margin method.past_task_reg=$past_margin_w method.future_task_reg=1 method.gamma=$gamma hydra=search model.path_selection_strategy=$pa model.prediction_mode=$pt wandb_prefix=routing_model_search_
             done
             done
             done
