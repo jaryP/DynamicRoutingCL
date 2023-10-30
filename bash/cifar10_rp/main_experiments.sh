cfc1cc74694db588e60ed15313a1994b5d3e84ab
jary.pomponi <jarypomponi@gmail.com>
2023-10-30 10:25:23 +0100

    config modified

--- bash/cifar10_rp/main_experiments.sh
+++ bash/cifar10_rp/main_experiments.sh
@@ -54,16 +54,16 @@ gdumb)
   python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=gdumb method.mem_size=2000 optimizer=adam  device="$DEVICE" experiment=base10
 ;;
 der)
-  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_200 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.2 method.beta=1
-  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_500 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.2 method.beta=0.8
-  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_1000 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.1 method.beta=0.8
-  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_2000 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.1 method.beta=1
+  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_200 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.2 method.beta=1 experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_500 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.2 method.beta=0.8  experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_1000 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.1 method.beta=0.8  experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=der_2000 optimizer=adam device=$DEVICE +model.head_classes=100 method.alpha=0.1 method.beta=1  experiment=base10
 ;;
 routing)
-  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=1 method.gamma=1
-  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=0.3 method.mem_size=500 method.gamma=1
-  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=0.3 method.mem_size=1000 method.gamma=1
-  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.1 method.past_margin=1 method.mem_size=2000 method.gamma=1
+  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=1 method.gamma=1  experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=0.3 method.mem_size=500 method.gamma=1  experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.01 method.past_margin=0.3 method.mem_size=1000 method.gamma=1  experiment=base10
+  python main.py +scenario=cil_cifar10_rp +model=routing_5l_convblock_p +training=cifar10_5 +method=routing_cifar10 optimizer=adam device=$DEVICE method.past_task_reg=0.1 method.past_margin=1 method.mem_size=2000 method.gamma=1  experiment=base10
 ;;
 #cope)
 #  python main.py +scenario=cil_cifar10_rp +model="$MODEL" +training=cifar10_5 +method=cope optimizer=adam  device="$DEVICE" experiment=base10 hydra.run.dir='./results/ci_cifar10/"$MODEL"/cope/cope_500'
