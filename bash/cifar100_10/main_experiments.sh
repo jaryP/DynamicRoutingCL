b468b616cc2a3d3e7426e09d7fe3790edb754b21
jary.pomponi <jarypomponi@gmail.com>
2023-11-30 00:01:35 +0100

    Bash files modified

--- bash/cifar100_10/main_experiments.sh
+++ bash/cifar100_10/main_experiments.sh
@@ -6,28 +6,28 @@ DEVICE=$3
 
 case $METHOD in
 gem)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_200 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_500 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_1000 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_2000 optimizer=adam  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_200 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_500 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_1000 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gem_2000 optimizer=sgd  device="$DEVICE"
 ;;
 #ewc)
-#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ewc_100 optimizer=adam  device="$DEVICE"
+#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ewc_100 optimizer=sgd  device="$DEVICE"
 #;;
 #oewc)
-#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=oewc_100 optimizer=adam  device="$DEVICE"
+#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=oewc_100 optimizer=sgd  device="$DEVICE"
 #;;
 #cml)
-#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cml_01 optimizer=adam  device="$DEVICE"
+#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cml_01 optimizer=sgd  device="$DEVICE"
 #;;
 naive)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100  +method=naive optimizer=adam  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100  +method=naive optimizer=sgd  device="$DEVICE"
 ;;
 cumulative)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cumulative optimizer=adam  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cumulative optimizer=sgd  device="$DEVICE"
 ;;
 #icarl)
-#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=icarl_2000 optimizer=adam  device="$DEVICE"
+#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=icarl_2000 optimizer=sgd  device="$DEVICE"
 #;;
 replay)
   python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=replay_200 optimizer=sgd  device="$DEVICE"
@@ -36,25 +36,25 @@ replay)
   python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=replay_2000 optimizer=sgd  device="$DEVICE"
 ;;
 ssil)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_200 optimizer=adam  device="$DEVICE" training.epochs=100
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_500 optimizer=adam  device="$DEVICE" training.epochs=100
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_1000 optimizer=adam  device="$DEVICE" training.epochs=100
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_2000 optimizer=adam  device="$DEVICE" training.epochs=100
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_200 optimizer=sgd  device="$DEVICE" training.epochs=100
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_500 optimizer=sgd  device="$DEVICE" training.epochs=100
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_1000 optimizer=sgd  device="$DEVICE" training.epochs=100
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=ssil_2000 optimizer=sgd  device="$DEVICE" training.epochs=100
 ;;
 er_ece)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=200 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=500 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=1000 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=2000 optimizer=adam  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=200 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=500 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=1000 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=er_ace_default method.mem_size=2000 optimizer=sgd  device="$DEVICE"
 ;;
 gdumb)
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=200 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=500 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=1000 optimizer=adam  device="$DEVICE"
-  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=2000 optimizer=adam  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=200 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=500 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=1000 optimizer=sgd  device="$DEVICE"
+  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=gdumb method.mem_size=2000 optimizer=sgd  device="$DEVICE"
 ;;
 #cope)
-#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cope optimizer=adam  device="$DEVICE" hydra.run.dir='./results/ci_cifar10/"$MODEL"/cope/cope_500'
+#  python main.py +scenario=cil_cifar100_10 model="$MODEL" +training=cifar100 +method=cope optimizer=sgd  device="$DEVICE" hydra.run.dir='./results/ci_cifar10/"$MODEL"/cope/cope_500'
 #;;
 *)
   echo -n "Unrecognized method"
---
+++ bash/cifar10_5/stab_plast_to_ablation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MODEL=$1
+DEVICE=$2
+
+for memory in 200 500 1000 2000
+do
+  for past_margin_w in 0.5 0.25 0.1 0.05 0.025 0.01
+  do
+    python main.py +scenario=cil_cifar10_5 model=$MODEL +training=cifar10_5 +method=margin_cifar10 device=$DEVICE method.mem_size=$memory method.past_task_reg=$past_margin_w method.gamma=1 +method.alpha=0 hydra=search +wadnb_tags=[sp_to_ablation] method.margin_type=adaptive +method.rehearsal_metric='kl' experiment=base1 head=margin_head
+  done
+done
