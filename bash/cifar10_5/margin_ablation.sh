3ba7faa94c253e649d917036eab1eecd36718a7a
jary.pomponi <jarypomponi@gmail.com>
2023-12-13 08:25:16 +0100

    Bash file modified

--- bash/cifar10_5/margin_ablation.sh
+++ bash/cifar10_5/margin_ablation.sh
@@ -68,7 +68,7 @@ do
     while (( ${num_jobs@P} >= ${max_jobs:-1} )); do
       wait -n
     done
-    python main.py +scenario=cil_cifar10_5 model=$MODEL +training=cifar10_5 +method=margin_cifar10 device=$DEVICE method.mem_size=$memory method.past_task_reg=$past_margin_w method.gamma=1 +method.alpha=0 hydra=search +wadnb_tags=[sp_to_ablation] method.margin_type=adaptive +method.rehearsal_metric='kl' experiment=base1 head=margin_head &
+    python main.py +scenario=cil_cifar10_5 model=$MODEL +training=cifar10_5 +method=margin device=$DEVICE method.mem_size=$memory method.past_task_reg=$past_margin_w method.gamma=1 +method.alpha=0 hydra=search +wadnb_tags=[sp_to_ablation] method.margin_type=adaptive +method.rehearsal_metric='kl' experiment=base1 head=margin_head &
   done
 done
 ;;
